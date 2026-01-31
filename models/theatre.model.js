5bdcd3a639f174008100da17b6fe7ca3b7856bcd
vineeta <220020057@iitdh.ac.in>
2026-01-31 20:28:30 +0530

    Feat: Added theatre schema

---
+++ models/theatre.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+/* defines the schema of the theatre resourse to be stored in the db */
+
+const theatreSchema = new mongoose.Schema({
+  name: {
+    type : String,
+    required:true
+  },
+  description : String ,
+  city : {
+    type : String , 
+    required: true
+  },
+  pincode : {
+    type : Number,
+    required : true
+  },
+  address : String
+} , {timestamps:true});
+
+const Theatre = mongoose.model('Theatre' , theatreSchema);
+module.exports = Theatre;
